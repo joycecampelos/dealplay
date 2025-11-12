25386f4c56bf9730de6badf068089cf021b2e4e8
Joyce Campelo <joycecampelos@gmail.com>
2025-11-12 03:28:03 -0300

    Update cypress.config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -34,6 +34,8 @@ export default defineConfig({
     video: false,
     screenshotOnRunFailure: false,
     supportFile: "cypress/support/e2e.js",
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
     setupNodeEvents(on, config) {
       on("task", {
         async createAdminUser() {
