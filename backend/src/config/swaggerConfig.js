d4e36dfc758f39e72daebd26f4afab4fb9de14e2
Joyce Campelo <joycecampelos@gmail.com>
2025-11-12 01:02:47 -0300

    Update swaggerConfig.js

--- backend/src/config/swaggerConfig.js
+++ backend/src/config/swaggerConfig.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const SERVER_URL = "http://localhost:3000";
+const SERVER_URL = "https://dealplay-backend.vercel.app";
 
 const options = {
   definition: {
